6daa33dbfa5635be9267e058a1cceb427075dadd
BeldyAdam <beldy.adam@gmail.com>
2023-03-29 09:08:53 +0200

    first commit

---
+++ adat.js
@@ -0,0 +1,6 @@
+export const ADATLISTA = [
+    {nev: "Dézi", fajta: "keverék", kor: "3"},
+    {nev: "Vladimir", fajta: "Orosz agár", kor: "3"},
+    {nev: "Hektor", fajta: "Bullmasztiff", kor: "4"},
+    {nev: "Dezső", fajta: "keverék", kor: "2"}
+];
---
+++ script.js
@@ -0,0 +1,33 @@
+import { ADATLISTA } from "./adat.js"
+
+$(function(){
+    tablazat();
+})
+
+function tablazat(lista){
+    let txt = "<div class= `table-responsive`>";
+    txt += "<table class=`table table-stripped table-boarded table-hover`>";
+    txt += "<thead class=`table-dark`><tr>";
+    for(const key in kulcsLista) {
+        txt += `<th id='${key}' > ${kulcsLista[key]}  ⇅<th/>`;        
+    }
+    txt += "<th></th></tr></thead>";
+    for (let index = 0; index < lista.length; index++) {
+        txt += "<tr>";
+        const object = lista[index];
+        for (const key in object) {
+            const element = object[key];
+            if (key === "nev") {
+                txt += `<th> ${element}</th>`;                
+            }
+            else{
+                txt += `<td> ${element}</td>`;
+            }
+        }
+        txt += '<td><button class="gomb torol" id="t${index}">✖</button></td>';
+        txt += "</tr>";
+    }
+    txt += "</table>";
+    txt += "</div>";
+    return txt;
+}
